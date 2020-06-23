c25563a61a180c5ad034541696af882c36bc550e
OleksiiKhatuntsev <kaelthasmafia@outlook.com>
2020-06-23 13:48:45 +0300

    add caesar cipher class

---
+++ caesar_cipher/caesar_cipher.py
@@ -0,0 +1,27 @@
+def get_ciphered_letter(alphabet, letter, shift_value=0):
+    index = alphabet.index(ord(letter))
+    if index + shift_value >= len(alphabet):
+        result_letter = alphabet[index + shift_value - len(alphabet)]
+    else:
+        result_letter = alphabet[index + shift_value]
+    return result_letter
+
+
+class CaesarCipher():
+
+    def __init__(self):
+        self.LOW_LETTERS = [letter for letter in range(ord('a'), ord('z') + 1)]
+        self.BIG_LETTERS = [letter for letter in range(ord('A'), ord('Z') + 1)]
+
+    def get_ciphered_text(self, text = "", shift_value = 0):
+        result_text = ""
+        for letter in text:
+            if letter.isupper():
+                result_text += chr(get_ciphered_letter(self.BIG_LETTERS, letter, shift_value))
+            elif letter.islower():
+                result_text += chr(get_ciphered_letter(self.LOW_LETTERS, letter, shift_value))
+        return result_text
+
+
+caesar_test = CaesarCipher()
+print(caesar_test.get_ciphered_text("zac", 1))
