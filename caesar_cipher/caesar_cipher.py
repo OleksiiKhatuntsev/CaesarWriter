90ee33dad70bba5d08770e154bcd058743d298f3
OleksiiKhatuntsev <kaelthasmafia@outlook.com>
2020-06-23 15:17:42 +0300

    full word programm without documentation

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -12,11 +12,14 @@ class CaesarCipher():
     def __init__(self):
         self.LOW_LETTERS = [letter for letter in range(ord('a'), ord('z') + 1)]
         self.BIG_LETTERS = [letter for letter in range(ord('A'), ord('Z') + 1)]
+        self.SPECIAL_SYMBOLS = ['\n', '\a', '\b', '\f', '\r', '\t']
 
     def get_ciphered_text(self, text = "", shift_value = 0):
         result_text = ""
         for letter in text:
-            if letter.isupper():
+            if letter in self.SPECIAL_SYMBOLS:
+                result_text += letter
+            elif letter.isupper():
                 result_text += chr(get_ciphered_letter(self.BIG_LETTERS, letter, shift_value))
             elif letter.islower():
                 result_text += chr(get_ciphered_letter(self.LOW_LETTERS, letter, shift_value))
